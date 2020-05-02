b307dfcc2cd588e3383ac29d8c510b315f4e9270
esla <slacowr@gmail.com>
2020-05-02 10:56:33 +0900

    Add another experimental LR schedule

--- config.py
+++ config.py
@@ -38,7 +38,7 @@ def learning_rate_mtd1(initial_lr, epoch):
     return initial_lr * math.pow(0.2, optim_factor)
 
 
-def learning_rate(initial_lr, epoch):
+def learning_rate_mine(initial_lr, epoch):
     if epoch < 20:
         initial_lr = initial_lr
     if epoch >= 20:
@@ -58,6 +58,18 @@ def learning_rate(initial_lr, epoch):
     if epoch > 160:
         initial_lr = 0.0004
     if epoch > 180:
+        initial_lr = 0.0001
+    return initial_lr
+
+
+def learning_rate(initial_lr, epoch):
+    if epoch < 150:
+        initial_lr = initial_lr
+    elif epoch >= 150:
+        initial_lr = 0.01
+    elif epoch > 250:
+        initial_lr = 0.001
+    elif epoch > 350:
         initial_lr = 0.0001
     return initial_lr
 
