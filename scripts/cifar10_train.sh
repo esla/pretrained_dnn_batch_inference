02934bd1728416cb584df3c49c8dca12cc0bd9c7
esla <slacowr@gmail.com>
2020-05-04 21:18:48 +0900

    Fix script

--- scripts/cifar10_train.sh
+++ scripts/cifar10_train.sh
@@ -9,11 +9,12 @@ python main.py \
 	--learning_type='multi_class'	\
 	--input_image_size=32   \
 	--datasets_class_folders_root_dir=/home/esla/research/datasets/cifar10-pngs-in-folders/cifar10  	\
-    	--dataset_class_type='class folders'	\
-        --datatransform='data_transform2'	\
+    --dataset_class_type='class folders'	\
+    --datatransform='data_transform2'	\
 	--batch_size=128    \
 	--input_image_size=32	\
 	--lr=0.1	\
+	--lr_scheduler=mtd2     \
 	--batch_size=128    \
 	--net_type=efficientnetB0	\
 	--dropout=0	\
